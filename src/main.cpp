055f898601bb0ee64f203e2f0c914e80d64b2e70
Autumn <dev@autumn.ninja>
2026-07-13 13:45:21 -0400

    Pass a value from one shader to another
    This change sets a color in the vertex shader which gets passed to our
    fragment shader and colors pixels based on that

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,20 @@ void processInput(GLFWwindow* window);
 // GLSL source code creates a vertex shader (can be moved to another file later)
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
     "}\0";
 
 // GLSL source code creates an orange fragment shader
 const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vertexColor;\n"
     "}\0";
 
 const char *fragmentShaderSourceYellow = "#version 330 core\n"
