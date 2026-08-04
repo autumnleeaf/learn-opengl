51d82b8921bbb199d39835730a2ff729afdc2fc4
Autumn <dev@autumn.ninja>
2026-08-04 14:24:25 -0400

    Add mesh class
    Create an abstraction class to handle drawing a mesh from a list of
    vertices and textures using a vertex element object

---
+++ src/mesh.hpp
@@ -0,0 +1,105 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <string>
+#include <vector>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#include "shader.hpp"
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec3 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+};
+
+class Mesh {
+    public:
+        std::vector<Vertex> vertices;
+        std::vector<unsigned int> indices;
+        std::vector<Texture> textures;
+
+        Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures) {
+            this->vertices = vertices;
+            this->indices = indices;
+            this->textures = textures;
+
+            setupMesh();
+        }
+
+        void Draw(Shader &shader) {
+            // Track the number of diffuse and specular textures separately
+            unsigned int diffuseNr = 1;
+            unsigned int specularNr = 1;
+
+            // Add the textures to the shader
+            for(unsigned int i = 0; i < textures.size(); i++) {
+                // Get the active texture by adding i to the 0th texture
+                glActiveTexture(GL_TEXTURE0 + i);
+
+                // Get the number string to append to the texture name
+                std::string number;
+                std::string name = textures[i].type;
+                if (name == "texture_diffuse") {
+                    number = std::to_string(diffuseNr++);
+                } else if (name == "texture_specular") {
+                    number = std::to_string(specularNr++);
+                }
+
+                // Pass in value
+                shader.setInt(("material." + name + number).c_str(), i);
+                glBindTexture(GL_TEXTURE_2D, textures[i].id);
+            }
+            glActiveTexture(GL_TEXTURE0);
+
+            // Draw mesh
+            glBindVertexArray(VAO);
+            glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+            glBindVertexArray(0);
+        }
+
+    private:
+        unsigned int VAO, VBO, EBO;
+
+        void setupMesh() {
+            // Initialize array objects and buffers
+            glGenVertexArrays(1, &VAO);
+            glGenBuffers(1, &VBO);
+            glGenBuffers(1, &EBO);
+
+            // Bind the vertex array object
+            glBindVertexArray(VAO);
+
+            // Vertex Array Buffer
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+            // Element Array Buffer
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+            // Set vertex positions
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+
+            // Set vertex normals
+            glEnableVertexAttribArray(1);
+            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+
+            // Set vertex texture coords
+            glEnableVertexAttribArray(2);
+            glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+
+            // Unbind vertex array object
+            glBindVertexArray(0);
+        }
+};
+
+#endif
